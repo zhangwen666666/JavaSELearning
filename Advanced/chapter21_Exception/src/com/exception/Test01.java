58f1c117f18dbbf6a1c84076aac3e1e40d9c18eb
zhangwen666666 <1813505899@qq.com>
2024-03-28 20:29:31 +0800

    异常Exception

--- Advanced/chapter21_Exception/src/com/exception/Test01.java
+++ Advanced/chapter21_Exception/src/com/exception/Test01.java
@@ -2,7 +2,7 @@ package com.exception;
 
 public class Test01 {
     public static void main(String[] args) {
-        int a = 10;
+        int a = 100;
         int b = 0;
         int c = a / b;
         System.out.println(c);
